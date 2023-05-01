88f34f52302f2208a694484720f2b441c4038275
Aleksandr Vladyko <mett0@bk.ru>
2023-05-01 23:37:36 +0700

    update file head

--- src/components/CalendarHead/CalendarHead.jsx
+++ src/components/CalendarHead/CalendarHead.jsx
@@ -6,57 +6,69 @@ const HeadContainer = styled.div`
 background-color: #eee;
 
 `;
-const NavDay = styled.td`
-  cursor: pointer;
-`;
+const WeekContainer = styled.div`
+display: grid;
+grid-template-columns: repeat(7, 1fr);
+height: 40px;
+margin-left: 60px;
+font-size: 20px;
+font-weight: bold;
 
-const NavDayName = styled.div`
-  text-align: center;
-  padding: 7px 0;
-  font-size: 16px;
-  font-weight: bold;
-  @media (max-width: 600px) {
-    font-size: 14px;
-  }
 `;
+const WeekOfDays = styled.div`
+margin-top: 5px;
+`;
+// const NavDay = styled.td`
+//   cursor: pointer;
+// `;
 
-const NavDayValue = styled.div`
-  text-align: center;
-  width: 50px;
-  height: 50px;
-  margin: auto;
-  line-height: 50px;
-  font-weight: bold;
-  font-size: 26px;
-  ${({ active }) =>
-    active &&
-    `
-    border-radius:25px;
-    background-color:red;
-    color: #fff;
-  `}
-  @media (max-width: 600px) {
-    width: 40px;
-    height: 40px;
-    line-height: 40px;
-    font-size: 20px;
-  }
-`;
+// const NavDayName = styled.div`
+//   text-align: center;
+//   padding: 7px 0;
+//   font-size: 16px;
+//   font-weight: bold;
+//   @media (max-width: 600px) {
+//     font-size: 14px;
+//   }
+// `;
 
-const NavTd = styled.td`
-  @media (max-width: 600px) {
-    height: 45px;
-  }
-`;
+// const NavDayValue = styled.div`
+//   text-align: center;
+//   width: 50px;
+//   height: 50px;
+//   margin: auto;
+//   line-height: 50px;
+//   font-weight: bold;
+//   font-size: 26px;
+//   ${({ active }) =>
+//     active &&
+//     `
+//     border-radius:25px;
+//     background-color:red;
+//     color: #fff;
+//   `}
+//   @media (max-width: 600px) {
+//     width: 40px;
+//     height: 40px;
+//     line-height: 40px;
+//     font-size: 20px;
+//   }
+// `;
 
-const NavCurrentMonth = styled.td`
-  font-size: 24px;
-  font-weight: bold;
-  text-align: center;
-  @media (max-width: 600px) {
-    font-size: 18px;
-  }
-`;
+// const NavTd = styled.td`
+//   @media (max-width: 600px) {
+//     height: 45px;
+//   }
+// `;
+
+// const NavCurrentMonth = styled.td`
+//   font-size: 24px;
+//   font-weight: bold;
+//   text-align: center;
+//   @media (max-width: 600px) {
+//     font-size: 18px;
+//   }
+// `;
 
 const NextWeek = styled.div`
   background-image: url(${arrow});
@@ -78,19 +90,30 @@ const PrevWeek = styled(NextWeek)`
 `;
 
 const CalendarHead = () => {
+  
   const [date, setDate] = useState(new Date());
-  const dayOfWeek = [];
-  const day = {
-    stringDay: null,
-    numberDay: null,
-  }
-  const first = date.getDate() - date.getDay() + 1;
-  const nextDay = first;
-  const firstDayofWeek = new Date(date.setDate(first )).toLocaleString('ru', {weekday: 'short'});
-     const lastDayofWeek = new Date(date.setDate(first + 6)).toLocaleString('ru', {weekday: 'short'});
+  const first = date.getDate() - date.getDay();
+  const lastDayofWeek = new Date(date.setDate(first + 6)).toLocaleString('ru', {weekday: 'short'});
+  // const firstDayOfWeek = new Date(date.setDate(first)).toLocaleString('ru', {weekday: 'short'});
+  
+  const getDays = [];
+  for(let day = 1; day <= 7; day++){
+    getDays.push(
+      new Date(date.setDate(first + day))
+  )}
 
+  const getDayOfWeek = getDays.map((getDay, index) => {
+    return <WeekOfDays key={index}>{getDay.toLocaleString('ru', {weekday: 'short'})}</WeekOfDays>
+  })
+  console.log(getDays);
    return (
     <HeadContainer>
+     <WeekContainer>
+      {getDayOfWeek}
+
+     </WeekContainer>
+
+      
       <NextWeek/>
       <PrevWeek/>
     </HeadContainer>
